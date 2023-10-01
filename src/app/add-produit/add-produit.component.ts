d76d3c41bd2096ce052e8fa90328d59d399122d8
sahli-abderazak <sahliabderazak53@gmail.com>
2023-10-02 00:11:44 +0100

    tp1

---
+++ src/app/add-produit/add-produit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-produit',
+  templateUrl: './add-produit.component.html',
+
+})
+export class AddProduitComponent {
+
+}
